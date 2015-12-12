ea05769545c61a9755bfe82f97daa5bb938543ce
Colin Haley <admin@colinhaley.net>
2015-12-11 20:45:10 -0500

    Full save method typed. Working on specific one now.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,11 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class GameController : MonoBehaviour
 {
+    public enum DataFiles
+    {
+        PlayerData,
+        LevelData
+    }
 
+    private DataFiles _dataFiles;
     public static GameController Controller;
 
+    // Make a singelton model controller with a static reference.
     void Awake()
     {
         if (Controller == null)
@@ -18,4 +30,27 @@ public class GameController : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    public void Save()
+    {
+        // Full save method. 
+        var enumNames = Enum.GetNames(typeof(DataFiles));
+        foreach (string enumName in enumNames)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + enumName, FileMode.Create);
+        }
+    }
+
+    public void Save(string test)
+    {
+        //Specific save file. How do I get the name of the enum specifically instead of looping?
+        var enumName = Enum.GetName(typeof (data));
+
+    }
+}
+
+class SaveData
+{
+    //Placeholder for data to save.
 }
